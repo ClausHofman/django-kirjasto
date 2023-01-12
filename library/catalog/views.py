dd38b4120932d02c31e3df13dc713fa253d2aa7f
ClausHofman <46158@edu.raseko.fi>
2023-01-12 20:28:01 +0200

    lisää kirja CreateView:lla

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book,Author,BookInstance,Genre,Language
+from django.views.generic import CreateView,DetailView
 
 
 # Create your views here.
@@ -20,3 +21,14 @@ def index(request):
     }
 
     return render(request,'catalog/index.html',context=context)
+
+# luo kirja CreateView:lla (malli Book), luo book_form.html, yhdistä näkymät urls.py (catalog/urls.py)
+class BookCreate(CreateView): # book_form.html
+    model = Book
+    fields = '__all__'
+
+# kun kirja on lisätty, oletuksena mene sen jälkeen DetailView sivulle:
+# luo book_detail.html ja lisää path urls.py
+class BookDetail(DetailView):
+    model = Book
+    # tai success_url = reverse_lazy('x:y')
